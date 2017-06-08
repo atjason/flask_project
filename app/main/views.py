7ee06a048412fdb10fb17853da178bbc69958d11
Jason <quietjason@gmail.com>
2017-06-08 16:41:18 +0800

    Add route for /user

--- app/main/views.py
+++ app/main/views.py
@@ -6,3 +6,9 @@ from ..models import User
 @main.route('/', methods=['GET', 'POST'])
 def index():
   return render_template('index.html')
+
+@main.route('/user/<name>', methods=['GET', 'POST'])
+def user(name):
+  user = User.query.filter_by(username=name).first()
+  username = user.username if user is not None else None
+  return render_template('user.html', name=username)
