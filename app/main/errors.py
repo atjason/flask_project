fb0f3cd830b1af8f2f2bbbca7bfe94b0fb3ed992
Jason <quietjason@gmail.com>
2017-06-08 15:58:42 +0800

    Correct a spell.

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,7 +2,7 @@ from flask import render_template
 from . import main
 
 @main.app_errorhandler(404)
-def page_not_fount(e):
+def page_not_found(e):
   return render_template('404.html'), 404
 
 @main.app_errorhandler(500)
