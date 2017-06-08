f0617f638577d01e8cf8a2373b4b45e8782840b1
Jason <quietjason@gmail.com>
2017-06-08 15:59:00 +0800

    Add SQLALCHEMY_TRACK_MODIFICATIONS = False flag

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 class Config:
   SECRET_KEY = os.environ.get("SECRET_KEY") or 'hard to guess string'
   SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
 
   @staticmethod
   def init_app(app):
