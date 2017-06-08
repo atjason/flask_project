7be1b1875f3ce88cbdb5f502f9a0b3b58b97fe27
Jason <quietjason@gmail.com>
2017-06-08 15:29:27 +0800

    Update app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+  app = Flask(__name__)
+  app.config.from_object(config[config_name])
+  config[config_name].init_app(app)
+
+  bootstrap.init_app(app)
+  db.init_app(app)
+
+  return app
